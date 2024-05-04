74ad406b74ec983f90cbc3e0b93686343aab62be
ne0mm <pablomaximiliano260@gmail.com>
2024-05-04 14:21:20 -0300

    mainpage/test delete

--- src/app/page.js
+++ src/app/page.js
@@ -1,58 +1,32 @@
 "use client"
 
-import { useEffect, useState } from 'react';
-
-function GyroscopeComponent() {
-
-  const [gyroscopeData, setGyroscopeData] = useState({ x: null, y: null, z: null });
-
-  const [ atData, setAtData] = useState({ x: 0, y: 0, z: 0 });
-
-  useEffect(() => {
-    if (window.DeviceOrientationEvent) {
-      window.addEventListener('deviceorientation', handleOrientation, true);
-      return () => {
-        window.removeEventListener('deviceorientation', handleOrientation, true);
-      };
-    } else {
-      console.log('Gyroscope not supported');
-    }
-  }, []);
-
-  const handleOrientation = (event) => {
-
-    setGyroscopeData({
-      x: event.beta,
-      y: event.gamma,
-      z: event.alpha
-    });
-
-  };
-
-  return (
-    <div>
-      <h2>Gyroscope Data:</h2>
-
-      <p>X: {atData.x}</p>
-      <p>Y: {atData.y}</p>
-      <p>Z: {atData.z}</p>
-
-      <h1>rData</h1>
-
-      <p>X: {gyroscopeData.x}</p>
-      <p>Y: {gyroscopeData.y}</p>
-      <p>Z: {gyroscopeData.z}</p>
-
-    </div>
-
-  );
-}
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const handleRedirect = (url) => {
+
+    router.push(url)
+
+  }
+
   return (
-    <div>
-      <h1>Next.js Gyroscope Example</h1>
-      <GyroscopeComponent />
+    <div className="flex flex-col">
+
+      <button
+        onClick={() => handleRedirect("/controller")}
+        className="bg-black text-white mb-8 mt-8"
+      >Controller</button>
+
+      <button
+        className="bg-[#000099] text-white"
+        onClick={() => handleRedirect("/receiver")}
+      >
+        Receiver
+      </button>
+
     </div>
   );
 }
